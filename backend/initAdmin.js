84d84e192b98348f28be6500ed7b5a0558a32af1
suthsonu <76761328+suthsonu@users.noreply.github.com>
2026-03-06 00:37:13 +0530

    initAdmin.js

--- backend/initAdmin.js
+++ backend/initAdmin.js
@@ -8,16 +8,18 @@ async function initAdmin() {
         const hash = await bcrypt.hash(password, 10);
 
         const existing = await db('admin_users').where({ email }).first();
-       if (!existing) {
-    await db('admin_users').insert({ email, password: hash });
-    console.log('Admin user created');
-} else {
-    await db('admin_users')
-        .where({ email })
-        .update({ password: hash });
 
-    console.log('Admin password updated successfully');
-}
+        if (!existing) {
+            await db('admin_users').insert({ email, password: hash });
+            console.log('Admin user created');
+        } else {
+            await db('admin_users')
+                .where({ email })
+                .update({ password: hash });
+
+            console.log('Admin password updated successfully');
+        }
+
     } catch (error) {
         console.error('Error seeding admin user:', error);
     } finally {
